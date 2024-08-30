41f7a5180751037d0fed6db75b9195c21e2c1cfd
nozma <co.panda.fc3s@gmail.com>
2024-08-30 23:14:59 +0900

    fix F_CPU

--- qmk_firmware/keyboards/keyball/drivers/pmw3360/pmw3360.c
+++ qmk_firmware/keyboards/keyball/drivers/pmw3360/pmw3360.c
@@ -23,7 +23,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "srom_0x81.c"
 
 #define PMW3360_SPI_MODE 3
+#ifdef F_CPU
 #define PMW3360_SPI_DIVISOR (F_CPU / PMW3360_CLOCKS)
+#else
+#define PMW3360_SPI_DIVISOR (clock_get_hz(clk_sys) / PMW3360_CLOCKS)
+#endif
 #define PMW3360_CLOCKS 2000000
 
 static bool motion_bursting = false;

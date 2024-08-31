268f4eeb5e7b730ba151b694f5fc7994e973d57d
nozma <co.panda.fc3s@gmail.com>
2024-09-01 02:54:15 +0900

    fix.

--- qmk_firmware/keyboards/keyball/drivers/pmw3360/pmw3360.c
+++ qmk_firmware/keyboards/keyball/drivers/pmw3360/pmw3360.c
@@ -113,10 +113,10 @@ bool pmw3360_motion_read(pmw3360_motion_t *d) {
     if ((mot & 0x88) != 0x80) {
         return false;
     }
-    d->x = pmw3360_reg_read(pmw3360_Delta_X_L);
-    d->x |= pmw3360_reg_read(pmw3360_Delta_X_H) << 8;
-    d->y = pmw3360_reg_read(pmw3360_Delta_Y_L);
-    d->y |= pmw3360_reg_read(pmw3360_Delta_Y_H) << 8;
+    d->y = pmw3360_reg_read(pmw3360_Delta_X_L);
+    d->y |= pmw3360_reg_read(pmw3360_Delta_X_H) << 8;
+    d->x = pmw3360_reg_read(pmw3360_Delta_Y_L);
+    d->x |= pmw3360_reg_read(pmw3360_Delta_Y_H) << 8;
     return true;
 }
 
@@ -134,10 +134,10 @@ bool pmw3360_motion_burst(pmw3360_motion_t *d) {
     wait_us(35);
     pmw3360_spi_read(); // skip MOT
     pmw3360_spi_read(); // skip Observation
-    d->x = pmw3360_spi_read();
-    d->x |= pmw3360_spi_read() << 8;
     d->y = pmw3360_spi_read();
     d->y |= pmw3360_spi_read() << 8;
+    d->x = pmw3360_spi_read();
+    d->x |= pmw3360_spi_read() << 8;
     pmw3360_spi_stop();
     wait_us(1);
     return true;

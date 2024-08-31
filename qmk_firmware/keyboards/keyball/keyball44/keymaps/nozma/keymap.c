8f0ae28d11cb0bd3ecfd82cc35baf84aa9b5f853
nozma <co.panda.fc3s@gmail.com>
2024-09-01 02:09:48 +0900

    fix.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/keymap.c
@@ -78,9 +78,6 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
         //keyball_oled_render_mymain();
-        oled_clear();
-        oled_write_ln("Debug Info:", false);
-        oled_render();  // 画面を更新する
     } else {
         draw_bongo(true);
     }

8bc99eb18405ff992c0d13d0a4d3d08831d3da89
nozma <co.panda.fc3s@gmail.com>
2024-08-31 18:46:24 +0900

    fix.

--- qmk_firmware/keyboards/keyball/drivers/pmw3360/pmw3360.c
+++ qmk_firmware/keyboards/keyball/drivers/pmw3360/pmw3360.c
@@ -226,7 +226,7 @@ bool pmw3360_init(void) {
 
 uint8_t pmw3360_srom_id = 0;
 
-void pmw3360_srom_upload(const uint8_t* firmware, size_t firmware_size) {
+void pmw3360_srom_upload(void) {
     pmw3360_reg_write(pmw3360_Config2, 0x00);
     pmw3360_reg_write(pmw3360_SROM_Enable, 0x1D);
     wait_us(10);
@@ -236,8 +236,8 @@ void pmw3360_srom_upload(const uint8_t* firmware, size_t firmware_size) {
     spi_write(pmw3360_SROM_Load_Burst | 0x80);
     wait_us(15);
 
-    for (size_t i = 0; i < firmware_size; i++) {
-        spi_write(firmware[i]);
+    for (size_t i = 0; i < pmw3360_srom_0x04.len; i++) {
+        spi_write(pgm_read_byte(&pmw3360_srom_0x04.data[i]));
         wait_us(15);
     }
 

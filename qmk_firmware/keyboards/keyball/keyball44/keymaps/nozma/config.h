53cd8df16ca532b026c6145589b732269d02fa69
nozma <co.panda.fc3s@gmail.com>
2024-09-01 23:35:37 +0900

    add layer.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/config.h
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 //#define AUTO_MOUSE_DEFAULT_LAYER 1
-#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #define KEYBALL_CPI_DEFAULT 800
 #define KEYBALL_SCROLL_DIV_DEFAULT 7
 

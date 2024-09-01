b7c0d903461e3d6c52207c0b14077a3a1dd0c7d7
nozma <co.panda.fc3s@gmail.com>
2024-09-01 11:00:30 +0900

    add debug message.

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -237,8 +237,9 @@ __attribute__((weak)) void keyball_on_apply_motion_to_mouse_scroll(keyball_motio
 }
 
 static void motion_to_mouse(keyball_motion_t *m, report_mouse_t *r, bool is_left, bool as_scroll) {
+    oled_write_ln("motion to mouse", false);
     if (as_scroll) {
-         oled_write_ln("as scroll on", false);
+        oled_write_ln("as scroll on", false);
         keyball_on_apply_motion_to_mouse_scroll(m, r, is_left);
     } else {
         keyball_on_apply_motion_to_mouse_move(m, r, is_left);

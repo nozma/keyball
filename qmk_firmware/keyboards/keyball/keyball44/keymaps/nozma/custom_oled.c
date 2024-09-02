afc99ae99fa294c409649b250bf183fb0f8f72c0
nozma <co.panda.fc3s@gmail.com>
2024-09-02 21:16:00 +0900

    adjust.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/custom_oled.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/custom_oled.c
@@ -170,9 +170,9 @@ static void print_wpm_type_count(void) {
     oled_write_ln(wpm, false);
     // type count表示
     static char type_count_str[7];
-    oled_write_ln_P(PSTR("Count"), false);
+    oled_write_P(PSTR("Count"), false);
     itoa(type_count, type_count_str, 10);
-    oled_write(type_count_str, false);
+    oled_write_ln(type_count_str, false);
 }
 
 // デフォルトページ表示

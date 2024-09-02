5f1a201d3a85646612a8d5045b5351d8029ec0d2
nozma <co.panda.fc3s@gmail.com>
2024-09-02 12:09:40 +0900

    fix.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/bongo.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/bongo.h
@@ -1,3 +1,5 @@
+#include QMK_KEYBOARD_H
+
 #define ANIM_FRAME_DURATION 75 // how long each frame lasts in ms
 #define ANIM_SIZE 636 // number of bytes in array, minimize for adequate firmware size, max is 1024
 #define IDLE_FRAMES 5

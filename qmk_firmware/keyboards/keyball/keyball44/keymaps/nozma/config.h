aae31bc16dcee3d3b2aa3080af0fea540dffa7e7
nozma <co.panda.fc3s@gmail.com>
2024-09-26 12:28:23 +0900

    change split usb timeout.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/config.h
@@ -78,8 +78,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
 /* define RP2040 PC起動時認識用（SPLIT_USB_DETECTが有効となっているため） */
-#define SPLIT_USB_TIMEOUT 2500 //Default 2000
-#define SPLIT_USB_TIMEOUT_POLL 25 //Default 10
+#define SPLIT_USB_TIMEOUT 2000 //Default 2000
+#define SPLIT_USB_TIMEOUT_POLL 10 //Default 10
 /* OLED driver and pins */
 #define HAL_USE_I2C TRUE
 #define I2C_DRIVER I2CD1

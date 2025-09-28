e86028b81e2366d88c8879a667a9cbb87b17a5ac
nozma <co.panda.fc3s@gmail.com>
2025-09-29 07:26:19 +0900

    Keyball44: レイヤー上限を8段に拡張

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/nozma/config.h
@@ -66,7 +66,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 //#define AUTO_MOUSE_DEFAULT_LAYER 1
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
 #define KEYBALL_CPI_DEFAULT 800
 #define KEYBALL_SCROLL_DIV_DEFAULT 7
 
